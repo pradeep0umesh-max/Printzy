e7dd108ad8e11da4a02e0eb09e1ad6f41c74b507
Printzy <pradeep0umesh@gmail.com>
2026-09-09 20:20:25 +0530

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,1 +1,9 @@
-js/supabase.js
+const SUPABASE_URL = "https://yxssyblhziyfbqxzjgxr.supabase.co";
+
+const SUPABASE_KEY =
+"sb_publishable_RhoD2kI568-a7ddA8iGhdA_dwJo_m_p";
+
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
